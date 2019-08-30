0757c68d41d1d3ead91177a87b8651eff6960f84
komeh <komehere@knou.ac.kr>
2019-08-30 18:28:22 +0900

    모바일로 크롤링

--- sele_tet.py
+++ sele_tet.py
@@ -3,17 +3,10 @@ from bs4 import BeautifulSoup
 
 path = "C:\\Users\\komeh\\Downloads\\chromedriver.exe"
 driver = webdriver.Chrome(path)
-driver.get('https://onoffmix.com/')
-search_box = driver.find_element_by_name("s")
-search_box.send_keys("밋업")
-search_box.submit()
-driver.find_element_by_xpath('//*[@id="content"]/div/section[2]/div[1]/ul[1]/li[2]/a').click()
+driver.get('https://m.onoffmix.com/event/main?s=%EB%B0%8B%EC%97%85')
 
 html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-all_divs = soup.find_all("h5.title.ellipsis")
-print(all_divs)
-
-#notices = soup.select('#content > div > section.event_main_area > ul > li > article.event_area.event_main > a')
-#for n in notices:
-#    print(n.text)
+soup = BeautifulSoup(html, 'html5lib')
+all_divs = soup.find_all('h1', {"class": "event_title"})
+for title in all_divs:
+    print(title.get_text())
